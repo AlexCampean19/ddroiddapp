2555cfe1e16778a8b261a242c8b570980fc0754d
AlexCampean19 <alex.campean24@gmail.com>
2023-10-12 15:31:09 +0300

    firstcomm

--- succes.js
+++ succes.js
@@ -1,6 +1,6 @@
 let dataform = JSON.parse(sessionStorage.getItem('formdata'));
 console.log(dataform)
 let template = '';
-template += '<div class="dateSucces"><p>First Name:</p><p>' + dataform.first_name + '</p></div><div class="dateSucces"><p>Last Name:</p><p>' + dataform.last_name + '</p></div><div class="dateSucces"><p>Telephone number:</p><p>' + dataform.phone + '</p></div><div class="dateSucces"><p>Email:</p><p>' + dataform.email + '</p></div><div class="dateSucces"><p>Address:</p><p>' + dataform.address + '</p><p>,' + dataform.address2 + '</p></div><div class="dateSucces"><p>Country:</p><p>' + dataform.country + '</p></div><div class="dateSucces"><p>State:</p><p>' + dataform.State + '</p></div><div class="dateSucces"><p>City:</p><p>' + dataform.City + '</p></div>';
+template += '<div class="dateSucces"><p>First Name:</p><p>' + dataform.first_name + '</p></div><div class="dateSucces"><p>Last Name:</p><p>' + dataform.last_name + '</p></div><div class="dateSucces"><p>Telephone number:</p><p>' + dataform.phone + '</p></div><div class="dateSucces"><p>Email:</p><p>' + dataform.email + '</p></div><div class="dateSucces"><p>Address:</p><p>' + dataform.address + '</p><p>,' + dataform.address2 + '</p></div><div class="dateSucces"><p>Country:</p><p>' + dataform.country + '</p></div><div class="dateSucces"><p>State:</p><p>' + dataform.state + '</p></div><div class="dateSucces"><p>City:</p><p>' + dataform.city + '</p></div>';
 console.log(template)
 jQuery('.summarytext').html(template)
